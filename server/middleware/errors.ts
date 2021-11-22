924ca2e6971e606d7fc293ab8afd0e1db08c175a
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-22 23:32:48 +0100

    added error middlewares

---
+++ server/middleware/errors.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+const chalk = require("chalk");
+const debug = require("debug")("file:server:errors");
+
+const notFoundErrorHandler = (req: express.Request, res: express.Response) => {
+  res.status(404).json({ error: "Endpoint not found" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalErrorHandler = (
+  error: any,
+  req: express.Request,
+  res: express.Response,
+  next: any
+) => {
+  debug(chalk.red("An error has ocurred: "), chalk.red(error.message));
+  const message = error.code ? error.message : "Wow";
+  res.status(error.code || 500).json({ error: message });
+};
+
+module.exports = {
+  notFoundErrorHandler,
+  generalErrorHandler,
+};

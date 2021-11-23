936b05792a99859c9c0c41bfede70a25a48ec494
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-23 17:44:04 +0100

    added user factory

---
+++ utils/Factories/usersFactory.ts
@@ -0,0 +1,20 @@
+import { Factory } from "fishery";
+import { internet, name } from "faker";
+import ObjectID from "bson-objectid";
+
+const factoryNewUsers = Factory.define(() => ({
+  name: name.findName(),
+  username: internet.userName(),
+  password: internet.password(),
+}));
+
+const factoryUsers = Factory.define(() => ({
+  id: new ObjectID(),
+  name: name.findName(),
+  username: internet.userName,
+  password: internet.password(),
+}));
+
+export const getRandomNewUser = () => factoryNewUsers.build();
+export const getRandomUser = () => factoryUsers.build();
+export const getRandomUsers = (total = 5) => factoryUsers.buildList(total);

77d3ba34d62c58ec0c45582f17922aff66e4a3df
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-23 23:36:45 +0100

    added login controller tests

--- server/controllers/userControllers.test.ts
+++ server/controllers/userControllers.test.ts
@@ -1,21 +1,28 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../../database/models/User";
 import newError from "../../utils/errorCreator";
-import { getRandomNewUser } from "../../utils/Factories/usersFactory";
+import {
+  getRandomNewUser,
+  getRandomUser,
+} from "../../utils/Factories/usersFactory";
 import {
   mockRequest,
   mockResponse,
   mockNextFunction,
 } from "../../utils/mocks/mockFunctionsForTests";
-import { registerUser } from "./userControllers";
+import { registerUser, loginUser } from "./userControllers";
 
 jest.setTimeout(50000);
 jest.mock("../../database/models/User");
 jest.mock("bcrypt");
+jest.mock("jsonwebtoken");
 
 let newUser: any;
+let user: any;
 beforeEach(() => {
   newUser = getRandomNewUser();
+  user = getRandomUser();
 });
 
 describe("Given a registerUser controller,", () => {
@@ -154,6 +161,138 @@ describe("Given a registerUser controller,", () => {
       const expectedError = newError(400, "User registration failed");
 
       await registerUser(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+describe("Given a loginUser controller,", () => {
+  describe("When it receives a body with an unexisting username,", () => {
+    test("Then it calls the next function", async () => {
+      User.findOne = jest.fn().mockResolvedValue(null);
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then it calls the next function with an error 401 'Wring Credentials'", async () => {
+      User.findOne = jest.fn().mockResolvedValue(null);
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      const expectedError = newError(401, "Wrong credentials");
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When it receives an existing user with a wrong password,", () => {
+    test("Then it calls the next function", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then it calls the next function with an error 401, 'Wrong Credentials'", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      const expectedError = newError(401, "Wrong credentials");
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When it receives an existing user with the right password,", () => {
+    test("Then it calls the method json", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+    test("Then it calls the method json with the token", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(res.json).toHaveBeenCalledWith({ token: "token" });
+    });
+  });
+  describe("When User.findOne rejects", () => {
+    test("Then it calls the next function", async () => {
+      User.findOne = jest.fn().mockRejectedValue(null);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then it calls the next function with an error 400 'User login failed'", async () => {
+      User.findOne = jest.fn().mockRejectedValue(null);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(400, "User login failed");
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When bcrypt.compare rejects", () => {
+    test("Then it calls the next function", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockRejectedValue(null);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then it calls the next function with an error 400 'User login failed'", async () => {
+      User.findOne = jest.fn().mockResolvedValue(user);
+      bcrypt.compare = jest.fn().mockRejectedValue(null);
+      jwt.sign = jest.fn().mockReturnValue("token");
+      const req = mockRequest(user);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(400, "User login failed");
+
+      await loginUser(req, res, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });

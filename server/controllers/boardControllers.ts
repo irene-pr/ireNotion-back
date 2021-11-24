95e1a7939aee6dcc2f5fef30bc43bccb45b55992
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-24 16:51:24 +0100

    added create board controller

---
+++ server/controllers/boardControllers.ts
@@ -0,0 +1,36 @@
+import chalk from "chalk";
+import Debug from "debug";
+import { NextFunction, Response } from "express";
+import Board from "../../database/models/Board";
+import User from "../../database/models/User";
+import newError from "../../utils/newError";
+import { RequestAuth } from "../middlewares/auth";
+
+const debug = Debug("irenotion:server:controllers:board");
+
+const createBoard = async (
+  req: RequestAuth,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const board = req.body;
+    const newBoard = await Board.create(board);
+    const user: any = await User.findByIdAndUpdate({ id: req.userId });
+    if (!user) {
+      debug(chalk.redBright("User not found"));
+      const error = newError(401, "User not found");
+      next(error);
+    } else {
+      user.boards.push(newBoard);
+      user.save(user);
+
+      res.status(204).json({ user, newBoard });
+    }
+  } catch {
+    const error = newError(401, "Could not create new board");
+    next(error);
+  }
+};
+
+export default createBoard;

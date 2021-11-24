69e323bc780bb5c5a2a8b55a5fefd8f955ab6a74
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-24 17:27:07 +0100

    added board endpoints to paths

--- paths/paths.ts
+++ paths/paths.ts
@@ -2,6 +2,9 @@ const paths = {
   userRoute: "/user",
   loginUser: "/login",
   registerUser: "/register",
+  boardRoute: "/boards",
+  createBoard: "/create",
+  deleteBoard: "/delete",
 };
 
 export default paths;

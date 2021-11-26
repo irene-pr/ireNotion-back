37fd3d5ab5e1cd231ca370ab7f099af4cab1c1ce
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-27 00:52:05 +0100

    added auth middleware to update note

--- server/middlewares/authorization.ts
+++ server/middlewares/authorization.ts
@@ -3,7 +3,7 @@ import Note from "../../database/models/Note";
 import { RequestAuth } from "../../utils/mocks/mockFunctionsForTests";
 import newError from "../../utils/newError";
 
-const authorizationForNoteDeletion = async (
+export const authorizationForNoteDeletion = async (
   req: RequestAuth,
   res: Response,
   next: NextFunction
@@ -14,22 +14,37 @@ const authorizationForNoteDeletion = async (
     if (!foundNote) {
       const error = newError(404, "Note not found");
       next(error);
-    }
-    if (`${foundNote.userId}` === req.userId) {
+    } else if (`${foundNote.userId}` === req.userId) {
       return next();
+    } else {
+      const error = newError(404, "User not allowed");
+      next(error);
     }
-
-    const error: {
-      message: string;
-      code?: number;
-    } = new Error("User not allowed");
-    error.code = 401;
-    next(error);
-  } catch (error) {
-    error.message = "Cannot search the activity";
-    error.code = 400;
+  } catch {
+    const error = newError(404, "Failed Authorization to access note deletion");
     next(error);
   }
 };
 
-export default authorizationForNoteDeletion;
+export const authorizationForNoteUpdate = async (
+  req: RequestAuth,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { idNote } = req.body;
+    const foundNote: any = await Note.findById(idNote);
+    if (!foundNote) {
+      const error = newError(404, "Note not found");
+      next(error);
+    } else if (`${foundNote.userId}` === req.userId) {
+      return next();
+    } else {
+      const error = newError(404, "User not allowed");
+      next(error);
+    }
+  } catch {
+    const error = newError(404, "Failed Authorization to access note update");
+    next(error);
+  }
+};

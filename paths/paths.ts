c64041ccf56740e210a41fc111b5da7c75a26a97
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-26 13:42:09 +0100

    added note paths

--- paths/paths.ts
+++ paths/paths.ts
@@ -5,6 +5,10 @@ const paths = {
   boardRoute: "/boards",
   createBoard: "/create",
   deleteBoard: "/delete/:idBoard",
+  noteRoute: "/note",
+  createNote: "/create",
+  deleteNote: "/delete/:idBoard/:idNote",
+  updateNote: "/update",
 };
 
 export default paths;

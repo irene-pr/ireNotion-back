7f4f92cfa30de316dde9e179fbe9386f88a4e56a
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-27 16:54:11 +0100

    implemented create and update note validators

--- server/schemas/noteSchemas.ts
+++ server/schemas/noteSchemas.ts
@@ -13,6 +13,17 @@ export const updateNoteValidation = {
   body: Joi.object({
     updatedNote: Joi.object({
       type: Joi.string().required(),
+      position: Joi.object({
+        x: Joi.number(),
+        y: Joi.number(),
+        h: Joi.number(),
+        w: Joi.number(),
+      }),
+      color: Joi.string(),
+      title: Joi.string(),
+      paragraph: Joi.string(),
+      list: Joi.array(),
+      file: Joi.string(),
     }),
     idNote: Joi.string().required(),
   }),

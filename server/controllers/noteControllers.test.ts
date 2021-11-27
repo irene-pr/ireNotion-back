9fa171518319bdf32b1b54b7a98023099e321fb2
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-27 20:11:33 +0100

    added delete note jest tests

--- server/controllers/noteControllers.test.ts
+++ server/controllers/noteControllers.test.ts
@@ -8,7 +8,7 @@ import {
   mockResponse,
 } from "../../utils/mocks/mockFunctionsForTests";
 import newError from "../../utils/newError";
-import { createNote } from "./noteControllers";
+import { createNote, deleteNote } from "./noteControllers";
 
 jest.mock("../../database/models/Board");
 jest.mock("../../database/models/Note");
@@ -153,6 +153,195 @@ describe("Given a createNote controller", () => {
       const expectedError = newError(400, "Could not create a new note");
 
       await createNote(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given a deleteNote controller", () => {
+  const params = {
+    idNote: ObjectID,
+    idBoard: ObjectID,
+  };
+  describe("When it is called", () => {
+    test("Then it executes", async () => {
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+    });
+  });
+  describe("When it receives a correct idBoard and idNote through params", () => {
+    test("Then calls the method json", async () => {
+      Note.findById = jest.fn().mockResolvedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+    test("Then emits a status", async () => {
+      Note.findById = jest.fn().mockResolvedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(res.status).toHaveBeenCalled();
+    });
+    test("Then emits a status 200", async () => {
+      Note.findById = jest.fn().mockResolvedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
+  });
+  describe("When it receives an unexisting idNote through params", () => {
+    test("Then calls the next function", async () => {
+      Note.findById = jest.fn().mockResolvedValue(null);
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then calls the next function with an error 404 'Note not found'", async () => {
+      Note.findById = jest.fn().mockResolvedValue(null);
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(404, "Note not found");
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When it receives an unexisting idBoard through params", () => {
+    test("Then calls the next function", async () => {
+      Note.findById = jest.fn().mockResolvedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue(null);
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then calls the next function with an error 404 'Board not found'", async () => {
+      Note.findById = jest.fn().mockResolvedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue(null);
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(404, "Board not found");
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When Note.findById rejects", () => {
+    test("Then calls the next function", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then calls the next function with an error 400 'Note deletion failed'", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(400, "Note deletion failed");
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When Board.updateOne rejects", () => {
+    test("Then calls the next function", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockRejectedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then calls the next function with an error 400 'Note deletion failed'", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockRejectedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockResolvedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(400, "Note deletion failed");
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+  describe("When Note.findByIdAndDelete rejects", () => {
+    test("Then calls the next function", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockRejectedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+
+      await deleteNote(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then calls the next function with an error 400 'Note deletion failed'", async () => {
+      Note.findById = jest.fn().mockRejectedValue({ note: "note" });
+      Board.updateOne = jest.fn().mockResolvedValue({ board: "board" });
+      Note.findByIdAndDelete = jest.fn().mockRejectedValue({ note: "note" });
+      const req = mockAuthRequest(null, null, params);
+      const res = mockResponse();
+      const next = mockNextFunction();
+      const expectedError = newError(400, "Note deletion failed");
+
+      await deleteNote(req, res, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
